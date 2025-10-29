add505848a361fd3cf071175f9ed443ed8f8b043
ebeleojeah <71762911+ebeleojeah@users.noreply.github.com>
2025-10-29 14:07:47 +0100

    Header MainContent Footer

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,23 +4,32 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage';
 
+// imports for the new components
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
 
-      <WelcomeMessage />
+      {/* New components in required order */}
+      <Header />
+      <MainContent />
+      <Footer />
 
       <h1>Vite + React</h1>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,6 +38,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
