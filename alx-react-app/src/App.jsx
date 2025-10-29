315319c87a5be7f20f8ed417140822fc19817e42
ebeleojeah <71762911+ebeleojeah@users.noreply.github.com>
2025-10-29 14:18:47 +0100

    Fixed components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './components/WelcomeMessage';
+
 
 // imports for the new components
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import Header from './components/Header.jsx';
+import MainContent from './components/MainContent.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
