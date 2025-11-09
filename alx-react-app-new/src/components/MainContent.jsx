2f4cad04b79a0694741009d2834a89476328cf1f
ebeleojeah <71762911+ebeleojeah@users.noreply.github.com>
2025-11-09 13:48:31 +0100

    add css style to react

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,7 +2,7 @@ function MainContent() {
   return (
     <main style={{ backgroundColor: 'lightgray', padding: '20px' }}>
       <h2 style={{ textAlign: 'center' }}>Main Content Area</h2>
-      <p>This is where the main content goes.</p>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }
