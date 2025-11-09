de697a9bd1096e28fb7de5c85ac52a481e4b3b3c
ebeleojeah <71762911+ebeleojeah@users.noreply.github.com>
2025-11-09 13:37:55 +0100

    Add inline CSS to react

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        backgroundColor: '#333',
+        color: '#fff',
+        textAlign: 'center',
+        padding: '10px',
+      }}
+    >
+      <p>&copy; 2025 My Favorite Cities. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,20 @@
+function MainContent() {
+  return (
+    <main
+      style={{
+        backgroundColor: '#e6f7ff',
+        padding: '20px',
+        minHeight: '200px',
+      }}
+    >
+      <h2 style={{ textAlign: 'center', color: '#333' }}>
+        Welcome to My Favorite Cities Page
+      </h2>
+      <p style={{ textAlign: 'center' }}>
+        Here you can find details about some amazing cities around the world!
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
