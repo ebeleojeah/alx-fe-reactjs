acecbdaa48ecccc17bac5b3339c616afd150dc05
ebeleojeah <71762911+ebeleojeah@users.noreply.github.com>
2025-11-23 19:17:26 +0100

    git check check

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,6 +1,12 @@
+import React from "react";
+import "./App.css";  // make sure this file exists
+
 function App() {
-return ( <div> <h1>GitHub User Search App</h1> </div>
-);
+  return (
+    <div>
+      <h1>GitHub User Search App</h1>
+    </div>
+  );
 }
 
 export default App;
