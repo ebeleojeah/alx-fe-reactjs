95c1fe7726958824b6ab1e92a89e0ad24d27fdf0
ebeleojeah <71762911+ebeleojeah@users.noreply.github.com>
2025-12-21 17:07:57 +0100

    try this time

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
-const Profile = () => {
+function Profile() {
   return (
     <div>
-      <h2>Profile Page</h2>
+      <h2>Profile</h2>
 
-      <nav>
-        <Link to="details">Details</Link> |{" "}
-        <Link to="settings">Settings</Link>
-      </nav>
-
-      <Outlet />
+      <Routes>
+        <Route path="/" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
     </div>
   );
-};
+}
 
 export default Profile;
